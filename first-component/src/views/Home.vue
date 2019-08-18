95fcdacb88db866c953078ea72d771e2f2ba3e4c
Maya Shavin <maya@cloudinary.com>
2019-08-18 15:14:28 +0300

    add tutorial section 2 - pokemonCard component

--- first-component/src/views/Home.vue
+++ first-component/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <pokemon-card />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import PokemonCard from '@/components/PokemonCard.vue';
 
 export default {
-  name: "home",
+  name: 'home',
   components: {
-    HelloWorld
-  }
+    PokemonCard,
+  },
 };
 </script>
